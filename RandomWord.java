033bbab9a14c170b1a72e53566b8e3fbd7c2581c
hung915 <hung09011995@gmail.com>
2025-11-03 13:17:23 +0700

    add hello world

---
+++ RandomWord.java
@@ -0,0 +1,19 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+
+    public static void main(String[] args) {
+        int index = 0;
+        String champion = "";
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            index++;
+            if (StdRandom.bernoulli(1.0 / index)) {
+                champion = word;
+            }
+        }
+        StdOut.println(champion);
+    }
+}
